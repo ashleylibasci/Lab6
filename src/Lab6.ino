c5d1f8079c8cfb34903f65c3a442eab59e39b033
Your Name <youremail@yourdomain.com>
2023-06-28 12:46:35 -0500

    update

--- src/Lab6.ino
+++ src/Lab6.ino
@@ -18,14 +18,23 @@ void loop()
 {
   uint64_t reading = analogRead(A4);
   double voltage = (reading * 3.3) / 4095.0;
-  double temperature = (voltage - 0.5) * 100;
+  double celsius = (voltage - 0.5) * 100;
+
+  double farenheit = (celsius * 9 / 5) + 32;
 
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println(temperature);
-  display.setCursor(37, 0);
+  display.println(celsius);
+  display.setCursor(33, 0);
   display.println("degrees celsius");
+  display.setCursor(0, 15);
+  display.println(farenheit);
+  display.setCursor(32, 15);
+  display.println("degrees");
+  display.setCursor(74.5, 15);
+  display.println("farenheit");
+
   display.display();
 }
