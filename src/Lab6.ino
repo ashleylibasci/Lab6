2dc84a702027175fa526b10a6d36cf023c9c3f78
Your Name <youremail@yourdomain.com>
2023-06-29 09:56:43 -0500

    update

--- src/Lab6.ino
+++ src/Lab6.ino
@@ -5,6 +5,11 @@ SYSTEM_THREAD(ENABLED);
 
 OledWingAdafruit display;
 
+uint64_t reading = analogRead(A4);
+double voltage = (reading * 3.3) / 4095.0;
+double celsius = (voltage - 0.5) * 100;
+double farenheit = (celsius * 9 / 5) + 32;
+
 void setup()
 {
   pinMode(A4, INPUT);
@@ -16,28 +21,31 @@ void setup()
 
 void loop()
 {
-  uint64_t reading = analogRead(A4);
-  double voltage = (reading * 3.3) / 4095.0;
-  double celsius = (voltage - 0.5) * 100;
+  celsiusFunction;
+  farenheitFunction;
+}
 
-  double farenheit = (celsius * 9 / 5) + 32;
-
-  display.loop();
-
-  display.clearDisplay();
+void celsiusFunction()
+{
+  display.clearDisplay(); // make the 2 blocks of code functions in like void loop to make it simpler
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.println(celsius);
   display.setCursor(33, 0);
   display.println("degrees celsius");
+  display.display();
+}
 
+void farenheitFunction()
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
   display.setCursor(0, 15);
   display.println(farenheit);
   display.setCursor(32, 15);
   display.println("degrees");
   display.setCursor(74.5, 15);
   display.println("farenheit");
-
   display.display();
 }
